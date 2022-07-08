cf719ac009ba5fa6ccb84f0e06a7a6f73e023a7f
Klanick <niklavr019@gmail.com>
2022-07-08 16:59:11 +0300

    Fix format in Days: 4 & 5

--- day_4/solution.cpp
+++ day_4/solution.cpp
@@ -1,6 +1,9 @@
+#include <vector>
+#include "iostream"
+
 class Solution {
 public:
-    bool isInterleave(const string & s1, const string & s2, const string & s3) {
+    static bool isInterleave(const std::string & s1, const std::string & s2, const std::string & s3) {
         if (s3.length() != s1.length() + s2.length()) {
             return false;
         }
@@ -15,7 +18,7 @@ public:
         for (int i = 0; i < s1.length(); i++) {
             d_next[0] = (prefix_s1 > i);
             for (int j = 1; j < s2.length() + 1; j++) {
-                if ((d[j] == true && s1[i] == s3[i+j]) || (d_next[j-1] == true && s2[j-1] == s3[i+j])) {
+                if ((d[j] && s1[i] == s3[i + j]) || (d_next[j - 1] && s2[j - 1] == s3[i + j])) {
                     d_next[j] = true;
                 } else {
                     d_next[j] = false;
@@ -27,7 +30,8 @@ public:
         return d[s2.length()];
     }
 
-    int common_prefix(const std::string & s1, const std::string & s2) {
+private:
+    static int common_prefix(const std::string & s1, const std::string & s2) {
         int prefix = 0;
         for (int i = 0; i < s1.length(); i++) {
             if (s1[i] == s2[i]) {
@@ -39,3 +43,10 @@ public:
         return prefix;
     }
 };
+
+int main() {
+    std::string s1, s2, s3;
+    std::cin >> s1 >> s2 >> s3;
+    std::cout << (Solution::isInterleave(s1, s2, s3) ? "true" : "false");
+    return 0;
+}
